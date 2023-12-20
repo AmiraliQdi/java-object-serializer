6505faa7648731d93af8ffb79b141894fdd52109
amirali <amirali.ghaedi@gmail.com>
2023-12-20 17:44:48 +0330

    Initial commit

---
+++ src/MyCipher.java
@@ -0,0 +1,14 @@
+public class MyCipher{
+
+    private final int[] array;
+
+    MyCipher(int[] array) {
+        this.array = array;
+    }
+
+    public String decode(String s) {
+        //TODO decode logic come here
+        return "decoded{"+ s + "}";
+    }
+
+}
