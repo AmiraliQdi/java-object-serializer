53aff847a9458d3a62f9b51c0b952f6cf753f554
amirali.qdi <amirali.ghaedi@gmail.com>
2023-12-23 00:38:11 +0330

    Serializer and Deserializer finished MyCipher now fully encode and decode keyGenerator fully functional

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        //String serializedFileName = "serialized";
-        //MyCipher myCipher = new MyCipher(new int[]{1, 2, 3},"KEY");
-        //Serializer.getInstance().serialize(myCipher,serializedFileName);
+        String serializedFileName = "serialized";
+        MyCipher myCipher = new MyCipher(new int[]{110, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ,14, 15, 16});
+        Serializer.getInstance().serialize(myCipher,serializedFileName);
+        System.out.println(myCipher.encode("a"));
 
-        System.out.println(MyCipher.getInstance().decode("adawda"));
+        //System.out.println(MyCipher.getInstance().decode("ESwbOCLzkhppSN89NTLdbw"));
     }
 }
--- src/MyCipher.java
+++ src/MyCipher.java
@@ -1,4 +1,11 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 public class MyCipher implements Serializable {
 
@@ -6,13 +13,12 @@ public class MyCipher implements Serializable {
 
     private static final MyCipher instance = (MyCipher) Deserializer.getInstance().deSerialize(SERIALIZED_FILE_NAME);
     private final int[] array;
+    private final static String initVector = "keymotherfucker!";
 
     //TODO remove this key value later as we can generate it from int array provided
-    private final String key;
 
-    MyCipher(int[] array,String key) {
+    MyCipher(int[] array) {
         this.array = array;
-        this.key = key;
     }
 
     public static MyCipher getInstance() {
@@ -24,18 +30,41 @@ public class MyCipher implements Serializable {
         this level of security can be reached.
         But the generating logic should not be a complex process,as it every time it can consume too much time
          */
-    private String keyGenerator() {
-        //TODO key generation logic here
-        return this.key;
+    private String keyGenerator() throws Exception {
+        if (array.length < 16) {
+            throw new IllegalArgumentException("Array must have at least 16 elements");
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 16; i++) {
+            char asciiChar = (char) array[i];
+            result.append(asciiChar);
+        }
+        return result.toString();
     }
 
-    public String decode(String s) {
-        //TODO decode logic come here
-        return "decoded{" + s + "}[" + keyGenerator() + "]";
+    public String generateRandomIV() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] iv = new byte[16];
+        secureRandom.nextBytes(iv);
+        String base64Encoded = Base64.getEncoder().encodeToString(iv);
+        return base64Encoded.substring(0, 16);
     }
 
-    public String encode(String s) {
-        //TODO encode logic come here
-        return "encoded{" + s + "}[" + keyGenerator() + "]";
+    public String decode(String encryptedData) throws Exception {
+        IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+        SecretKeySpec skeySpec = new SecretKeySpec(keyGenerator().getBytes("UTF-8"), "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+        byte[] original = cipher.doFinal(Base64.getUrlDecoder().decode(encryptedData));
+        return new String(original);
+    }
+
+    public String encode(String data) throws Exception {
+        IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+        SecretKeySpec skeySpec = new SecretKeySpec(keyGenerator().getBytes("UTF-8"), "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+        return Base64.getUrlEncoder().encodeToString(encrypted).replaceAll("=","");
     }
 }
